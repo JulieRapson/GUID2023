16715de3adb752f2e2e792b588cc03b1b8feb537
JulieRapson <rapson_julie@columbusstate.edu>
2023-02-02 10:16:57 -0500

    Burger project v1

---
+++ BurgerSpecialsJR/frmBurgers.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmBurgers
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblConfirmation_Click(sender As Object, e As EventArgs) Handles lblConfirmation.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+End Class
